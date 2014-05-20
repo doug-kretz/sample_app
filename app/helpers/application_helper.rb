b7e82baf0356d6a3d618ef7e6269dbfe8fd5905b
doug-kretz <doug.kretz@gmail.com>
2014-05-20 13:04:11 -0700

    switch to postgres database

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Returns the full titleon a per-page basis
+  def full_title(page_title)
+    base_title = "Ruby on RailsTutorial Sample App"
+    if page_tite.empty?
+      base_title
+    else
+      "#{base_tite} | #{page_title}"
+    end
+  end
 end
